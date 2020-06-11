9bdc7a78dd5c8494934530f920fd2a45bfbd9b28
gnuoy-oh <oh.my00418@gmail.com>
2020-06-11 14:58:15 +0900

    초급 강좌 마무리중

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <form action="" v-on:submit="submitForm">
+  <form action="" v-on:submit.prevent="submitForm">
     <div>
       <label for="userName">id: </label>
       <input type="text" id="userName" v-model="userName" />
@@ -15,6 +15,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   // component간에 데이터가 공유되지 않도록, 새 객체를 return 하는 구문으로 만들어준다.
   data: function(){
@@ -24,12 +26,26 @@ export default {
     }
   },
   methods: {
-    submitForm: function(event){
-      event.preventDefault();
-      
+    submitForm: function(){
+      // vanilla JS 방식
+      // event.preventDefault();
       console.log(this.userName, this.userPw);
-      
 
+      // 서버에 전송하기
+      // npm i axios -> 브라우저에서 서버로 데이터를 주고받기 위한 용도의 Library
+      // post -> 데이터를 생성하거나 조작하는 Method
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        userName: this.userName,
+        userPw: this.userPw
+      }
+      axios.post(url, data)
+      .then(function(response){
+        console.log(response)
+      })
+      .catch(function(error){
+        console.log(error)
+      })
     }
   }
 }
