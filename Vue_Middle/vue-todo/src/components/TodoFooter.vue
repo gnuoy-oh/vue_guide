1a1beda6ee52e127b8ba66cb54fc78f2a3c99938
gnuoy-oh <oh.my00418@gmail.com>
2020-06-16 14:17:31 +0900

    add

---
+++ Vue_Middle/vue-todo/src/components/TodoFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Footer Footer
+  </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style>
+</style>
---
+++ Vue_Middle/vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>Header</h1>
+  </header>
+</template>
+
+
+// 컴포넌트 안에서만 유효하도록 설정하는 것 (컴포넌트가 아니면 css가 적용되지 않는다.)
+<style scoped>
+h1{
+  color: #262626;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+
+</style>
