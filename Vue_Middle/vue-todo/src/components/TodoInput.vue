4a42b4ad406f94279f4e47a5ae64366f5e900043
gnuoy-oh <oh.my00418@gmail.com>
2020-08-18 16:37:37 +0900

    모달 slot 사용 방법 정리중

--- Vue_Middle/vue-todo/src/components/TodoInput.vue
+++ Vue_Middle/vue-todo/src/components/TodoInput.vue
@@ -3,19 +3,36 @@
     <!-- ** v-model: input form 에 입력된 text 값이 vue instance 안에 바로바로 mapping하는 역할을 한다. -->
     <!-- Instance에서 data 변경이 서로 동기화가 되는 것을 확인할 수 있다. -->
     <!-- === two way binding -->
-     <input type="text" v-model="newTodoItem" v-on:keyup.enter="addToDo" />
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addToDo" />
     <!-- <button v-on:click="addTodo">add</button> -->
     <span class="addContainer" v-on:click="addToDo">
       <i class="fas fa-plus addBtn"></i>
     </span>
+
+    <Modal v-if="showModal" @close="showModal = false">
+      <!--
+      you can use custom content here to overwrite
+      default content
+      -->
+      <h3 slot="header">경고!</h3>
+
+      <p slot="body"> hoho </p>
+
+      <div slot="footer"> Lorem, ipsum. </div>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
 export default {
+  components: {
+    Modal: Modal,
+  },
   data() {
     return {
       newTodoItem: "",
+      showModal: false,
     };
   },
   methods: {
@@ -23,10 +40,14 @@ export default {
     addToDo() {
       if (this.newTodoItem !== "") {
         console.log(this.newTodoItem);
-        // this.$emit('이벤트 이름', 인자1, 인자2 ...) 
+        // this.$emit('이벤트 이름', 인자1, 인자2 ...)
         // v-on 이벤트가 발생했을 경우. addItem이라는 이벤트가 발생하고 this.newTodoItem인자를 상위로 보내준다.($emit)
-         this.$emit('addItem', this.newTodoItem);
+        this.$emit("addItem", this.newTodoItem);
         this.clearInput();
+      }
+      // 입력 값이 비어있는 경우,
+      else {
+        this.showModal = !this.showModal;
       }
     },
     // input value 값을 지운다.
